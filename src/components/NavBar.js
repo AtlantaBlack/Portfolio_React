7f5820bb33665d8091725091500a7e9250299528
Atlanta Black <97859552+AtlantaBlack@users.noreply.github.com>
2022-07-10 15:10:14 +1000

    added navbar

---
+++ src/components/NavBar.js
@@ -0,0 +1,48 @@
+const NavBar = ({ currentPage, handlePageChange }) => {
+  return (
+    <ul>
+      <li>
+        <a
+          href="#about"
+          onClick={() => handlePageChange('About')}
+          className={currentPage === 'About' ? 'link-active' : 'link-inactive'}
+        >
+          About
+        </a>
+      </li>
+      <li>
+        <a
+          href="#portfolio"
+          onClick={() => handlePageChange('Portfolio')}
+          className={
+            currentPage === 'Portfolio' ? 'link-active' : 'link-inactive'
+          }
+        >
+          Portfolio
+        </a>
+      </li>
+      <li>
+        <a
+          href="#resume"
+          onClick={() => handlePageChange('Resume')}
+          className={currentPage === 'Resume' ? 'link-active' : 'link-inactive'}
+        >
+          Resume
+        </a>
+      </li>
+      <li>
+        <a
+          href="#contact"
+          onClick={() => handlePageChange('Contact')}
+          className={
+            currentPage === 'Contact' ? 'link-active' : 'link-inactive'
+          }
+        >
+          Contact
+        </a>
+      </li>
+    </ul>
+  );
+};
+
+export default NavBar;
